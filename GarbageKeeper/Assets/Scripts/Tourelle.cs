b312c8f3f968b0a25af162f0953aaea1d05c315e
youri.gicquel <youri.gicquel@gmail.com>
2019-11-09 15:41:03 +0100

    Premiere version du lookAt des tourelles  NON TESTE

--- GarbageKeeper/Assets/Scripts/Tourelle.cs
+++ GarbageKeeper/Assets/Scripts/Tourelle.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Tourelle : MonoBehaviour
 {
+    public transform[] ennemiPos;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void LookAtEnnemi(transform closestEnnemi)
+    {
+        transform.LookAt(closestEnnemi);
+    }
+
+    int GetClosestEnnemiId()
+    {
+        float dist = -1;
+        ret = -1;
+        for (int i = 0; i < ennemiPos.Length; ++i)
+        {
+            float distance = Vector3.Distance(ennemiPos[i], transform.position);
+            if (-1 == dist ||  distance < dist)
+            {
+                dist = distance;
+            }
+        }
+        return ret;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        int closest = GetClosestEnnemiId();
+        LookAtEnnemi(ennemiPos[closest]);
     }
 }
