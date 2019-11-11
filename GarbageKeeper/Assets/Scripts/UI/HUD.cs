f717220e4ca25dd40b69528ddfdcaabe6575319a
LilCartell <lilian.cartellier@gmail.com>
2019-11-11 12:03:24 +0100

    Added hud health bar

--- GarbageKeeper/Assets/Scripts/UI/HUD.cs
+++ GarbageKeeper/Assets/Scripts/UI/HUD.cs
@@ -20,7 +20,7 @@ public class HUD : MonoBehaviour
         _timeElapsed += Time.deltaTime;
         TimeSpan t = TimeSpan.FromSeconds(_timeElapsed);
 
-        timer.text = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}",
+        timer.text = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D2}",
                         t.Hours,
                         t.Minutes,
                         t.Seconds,
