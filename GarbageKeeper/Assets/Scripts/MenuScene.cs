84d930762ed320712ed36755d9b865417bcf99eb
LilCartell <lilian.cartellier@gmail.com>
2019-11-11 13:32:18 +0100

    Added menu and game over

---
+++ GarbageKeeper/Assets/Scripts/MenuScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MapScene");
+    }
+}
--- GarbageKeeper/Assets/Scripts/UI/HUD.cs
+++ GarbageKeeper/Assets/Scripts/UI/HUD.cs
@@ -7,18 +7,18 @@ public class HUD : MonoBehaviour
     public Image healthBar;
     public Text timer;
 
-    private float _timeElapsed;
+    public float TimeElapsed { get; private set; }
 
     private void Awake()
     {
-        _timeElapsed = 0;
+        TimeElapsed = 0;
     }
 
     public void Update()
     {
         healthBar.fillAmount = GameManager.Instance.CurrentLife / Settings.Instance.lifeMax;
-        _timeElapsed += Time.deltaTime;
-        TimeSpan t = TimeSpan.FromSeconds(_timeElapsed);
+        TimeElapsed += Time.deltaTime;
+        TimeSpan t = TimeSpan.FromSeconds(TimeElapsed);
 
         timer.text = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D2}",
                         t.Hours,
