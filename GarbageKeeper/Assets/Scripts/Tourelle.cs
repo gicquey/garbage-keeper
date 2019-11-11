a4463cf710b13b5babaef1a29b31e40d8a6c904d
flboissy <boissy.florent@gmail.com>
2019-11-11 11:29:52 +0100

    turret canvas, update icons

--- GarbageKeeper/Assets/Scripts/Tourelle.cs
+++ GarbageKeeper/Assets/Scripts/Tourelle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using static Projectile;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tourelle : MonoBehaviour
 {
@@ -9,15 +10,25 @@ public class Tourelle : MonoBehaviour
     private float timer = 0f;
     public Transform projectilSpawn;
     public Animator poubelleAnimator;
+    public Image ammoTypeFeedBack;
+
+    public Sprite regular;
+    public Sprite battery;
+    public Sprite clothes;
+    public Sprite explosive;
+    public Sprite poison;
+    public Sprite puddle;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        ammoTypeFeedBack.sprite = Resources.Load<Sprite>("Sprite/regular");
     }
 
     void LookAtEnnemi(Transform closestEnnemi)
     {
-        transform.LookAt(new Vector3(closestEnnemi.position.x, transform.position.y, closestEnnemi.position.z) );
+        transform.LookAt(new Vector3(closestEnnemi.position.x, transform.position.y, closestEnnemi.position.z));
     }
 
     int GetClosestEnnemiId()
@@ -27,7 +38,7 @@ public class Tourelle : MonoBehaviour
         for (int i = 0; i < WaveManager.Instance.AliveEnnemies.Count; ++i)
         {
             float distance = Vector3.Distance(WaveManager.Instance.AliveEnnemies[i].transform.position, transform.position);
-            if (-1 == dist ||  distance < dist)
+            if (-1 == dist || distance < dist)
             {
                 dist = distance;
                 ret = i;
@@ -40,19 +51,26 @@ public class Tourelle : MonoBehaviour
     {
         switch (ammotype)
         {
-            case(Settings.AmmoType.regular):
-                return Instantiate(((GameObject)Resources.Load("Prefabs/Projectiles/NormalProjectile")).GetComponent<Projectile>(), projectilSpawn.position,transform.rotation).GetComponent<Projectile>();
-            case(Settings.AmmoType.battery):
+            case (Settings.AmmoType.regular):
+                ammoTypeFeedBack.sprite = regular;
+                return Instantiate(((GameObject)Resources.Load("Prefabs/Projectiles/NormalProjectile")).GetComponent<Projectile>(), projectilSpawn.position, transform.rotation).GetComponent<Projectile>();
+            case (Settings.AmmoType.battery):
+                ammoTypeFeedBack.sprite = battery;
                 return Instantiate(((GameObject)Resources.Load("Prefabs/Projectiles/BatteryProjectile")).GetComponent<Projectile>(), projectilSpawn.position, transform.rotation).GetComponent<Projectile>();
             case (Settings.AmmoType.clothes):
+                ammoTypeFeedBack.sprite = clothes;
                 return Instantiate(((GameObject)Resources.Load("Prefabs/Projectiles/ClothesProjectile")).GetComponent<Projectile>(), projectilSpawn.position, transform.rotation).GetComponent<Projectile>();
             case (Settings.AmmoType.explosive):
+                ammoTypeFeedBack.sprite = explosive;
                 return Instantiate(((GameObject)Resources.Load("Prefabs/Projectiles/ExplosiveProjectile")).GetComponent<Projectile>(), projectilSpawn.position, transform.rotation).GetComponent<Projectile>();
             case (Settings.AmmoType.poison):
+                ammoTypeFeedBack.sprite = poison;
                 return Instantiate(((GameObject)Resources.Load("Prefabs/Projectiles/PoisonProjectile")).GetComponent<Projectile>(), projectilSpawn.position, transform.rotation).GetComponent<Projectile>();
             case (Settings.AmmoType.puddle):
+                ammoTypeFeedBack.sprite = puddle;
                 return Instantiate(((GameObject)Resources.Load("Prefabs/Projectiles/PuddleProjectile")).GetComponent<Projectile>(), projectilSpawn.position, transform.rotation).GetComponent<Projectile>();
             default:
+                ammoTypeFeedBack.sprite = regular;
                 return Instantiate(((GameObject)Resources.Load("Prefabs/Projectiles/NormalProjectile")).GetComponent<Projectile>(), projectilSpawn.position, transform.rotation).GetComponent<Projectile>();
         }
     }
@@ -62,7 +80,7 @@ public class Tourelle : MonoBehaviour
         poubelleAnimator.SetTrigger("Shoot");
 
         StartCoroutine(ShootCoroutine(e, poubelleAnimator));
-       
+
     }
 
     IEnumerator ShootCoroutine(Ennemi e, Animator anim)
